d28b449877fb618323c2c72a10baf94e29395465
Duvan Rivera Montealegre <Duvan.Rivera@huhmp.local>
2022-08-25 07:40:35 -0500

    despliegue ecollantas

---
+++ inventario/ecollantas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcollantasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ecollantas'
---
+++ inventario/ecollantas/models.py
@@ -0,0 +1,29 @@
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+GARANTIA=(
+    ('1','Si'),
+    ('2','No'),
+)
+
+
+class Categoria(models.Model):
+    tipo= models.CharField(max_length=30, unique=True)
+
+class Marca(models.Model):
+    tipo: models.CharField(max_length=30, unique=True)
+
+class Rin(models.Model):
+    tipo: models.CharField(max_length=30, unique=True)
+    
+class productos(models.Model):
+    id = models.AutoField(primary_key=True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    rin = models.ForeignKey(Rin, on_delete = models.CASCADE)
+    marca = models.CharField(max_length=20)
+    referencia = models.CharField(max_length=30)
+    garantia = models.CharField(max_length=30, null=True,choices=GARANTIA)
+    descripcion = models.CharField(max_length=250)
+    precio = models.FloatField()
+    
